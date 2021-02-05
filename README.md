b517889294f1bf0f97dc34d43f1581278a6223e4
Krish Garg <65218584+KrishGarg@users.noreply.github.com>
2021-02-05 15:29:26 +0530

    A start to the spammer

---
+++ README.md
@@ -0,0 +1,6 @@
+# Discord Spammer
+## You can use this to prank your friends. All you have to do is run the script and it will ask you somethings like number of messages per cycle and how many cycles etc. 
+### Cycles are groups of messages sent together. There are time gaps between each cycle because discord limits mass sending of messages. 
+### Messages per cycle should be less than 6 according to me but I haven't tested above that so test at your own risk.
+### I don't steal any of your data and if you don't beleive me then you can verify the code yourself.
+# I have written the code myself and anyone can use it but no one is allowed to sell this code or something similar. Copyright goes to me.
---
+++ Spam.py
@@ -0,0 +1,27 @@
+import pyautogui
+import time
+
+
+#declaring variables
+looper = 0 
+msgs = 0
+
+timeneeded = int(input("Time you need to switch to discord (in seconds): "))
+messages = int(input("How many messages per cycles: "))
+cycles = int(input("How many cycles of messages: "))
+print("Quickly, you have", timeneeded, "seconds to open discord and the channel/dm you want to spam in!")
+
+def spam():                       #I mean if you don't understand even this, duh.
+    pyautogui.typewrite("Test 5 messages, 4 cycles.")
+    pyautogui.press("enter")
+
+#gives time to switch to discord
+time.sleep(timeneeded)
+
+while looper < cycles:            #change the integer to the number of times you want to do 5-message cycles
+    while msgs < messages:        #how many messages per cycles
+        spam()                    #spam :evil:
+        msgs+=1                   #to actually end 1 cycle 
+    time.sleep(3)                 #time break between cycles to prevent limiting by discord
+    msgs=0                        #resetting the cycle
+    looper+=1                     #increasing the looper variable to limit the repeatations
